eb6c1203207be5bbf26d0e38a44681aac0da92fc
krishnapawar26 <121858190+krishnapawar26@users.noreply.github.com>
2025-01-09 23:55:01 +0530

    Create database.py

---
+++ database.py
@@ -0,0 +1,20 @@
+import sqlite3
+
+class Database:
+    def __init__(self, db_name):
+        self.conn = sqlite3.connect(db_name)
+        self.cursor = self.conn.cursor()
+
+    def execute_query(self, query, params=None):
+        if params:
+            self.cursor.execute(query, params)
+        else:
+            self.cursor.execute(query)
+        self.conn.commit()
+
+    def fetchall(self, query, params=None):
+        if params:
+            self.cursor.execute(query, params)
+        else:
+            self.cursor.execute(query)
+        return self.cursor.fetchall()
